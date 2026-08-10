95add30c6c241b3138527e81eced15bc634e4c0a
Kaviya <kaviya0898@gmail.com>
2026-08-10 11:41:56 +0530

    Initial project setup

---
+++ src/test/java/com/staywise/proximity_service/ProximityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.staywise.proximity_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProximityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
